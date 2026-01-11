dd1862839a470fbfca5ed843e099ae2d2b98634b
Antonio Palacio <antonioqw3@gmail.com>
2026-01-11 09:23:41 -0600

    Directivas y estructuras de control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-app';
+  title: string = 'Hola Mundo Angular!';
+  users: string[] = ['Antonio', 'Alberto', 'David'];
+  visible: boolean = false;
+
+  setVisible(): void {
+    this.visible = this.visible ? false : true;
+    console.log(this.visible)
+  }
 }
