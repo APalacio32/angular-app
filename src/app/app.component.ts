9be0cfe7e585b43acbe26722271299bc64025808
Antonio Palacio <antonioqw3@gmail.com>
2026-01-11 10:39:17 -0600

    Componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { CounterComponent } from "./counter/counter.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
